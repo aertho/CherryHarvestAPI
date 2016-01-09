f0bdeb39063431c53708bbd89e93ef5ba53b958b
aertho <thomasae@utas.edu.au>
2016-01-09 23:30:18 +1100

    totday resource added

--- CherryHarvestAPI/resources/today.py
+++ CherryHarvestAPI/resources/today.py
@@ -7,11 +7,11 @@ from sqlalchemy.orm import Load
 
 class Today(Resource):
     def get(self):
-        block_totals = [{b.variety: sum([l.weight for l in Lug.query.all() if l.block == b and l.orchard_load.arrival_time == date.today()])} for b in Block.query.all()]
-        total = sum([l.net_weight for l in OrchardLoad.query.all() if l.arrival_time.date() == date.today()])
-        load_times = len([l for l in OrchardLoad.query.all() if l.arrival_time.date() == date.today()])
+        blocks = [{'variety' : b.variety, 'weight_today': sum([l.weight for l in Lug.query.all() if l.block == b and l.orchard_load.arrival_time == date.today()])} for b in Block.query.all()]
+        total_today = sum([l.net_weight for l in OrchardLoad.query.all() if l.arrival_time.date() == date.today()])
+        loads = [{'time' :l.arrival_time, 'weight' : l.net_weight} for l in OrchardLoad.query.all() if l.arrival_time.date() == date.today()]
         pickers = [{'{} {}'.format(p.first_name, p.last_name): p.daily_total()} for p in Picker.query.all() if p.daily_total()]
-        return {'total' : total,
-                'block_totals': block_totals,
-                'load_times': load_times,
+        return {'total_today' : total_today,
+                'blocks': blocks,
+                'loads': loads,
                 'pickers' : pickers}
