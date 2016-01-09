f71e178bc2906d6d588dceb6ae9c115a0b2aeeaa
aertho <thomasae@utas.edu.au>
2016-01-09 16:53:05 +1100

    farm load

--- CherryHarvestAPI/resources/farm_load.py
+++ CherryHarvestAPI/resources/farm_load.py
@@ -1,29 +1,65 @@
 from copy import copy
 
-from CherryHarvestAPI import models, app
+from CherryHarvestAPI import models
 from CherryHarvestAPI.database import db_session
 from CherryHarvestAPI.resources import load
 from CherryHarvestAPI.resources.common import simple_lug_fields
+from dateutil import parser
 from flask.ext.restful import Resource, marshal_with, reqparse, fields, abort
 from sqlalchemy.exc import IntegrityError
 import regex
 
 _farm_load_fields = copy(load.outer_load_fields)
-_farm_load_fields['destination'] = fields.String
 # _farm_load_fields['lugs'].endpoint = 'farm_load_lugs'
-
-_farm_load_parser = copy(load.outer_load_parser)
-# _farm_load_parser.add_argument('destination')
 
 
 
 class FarmLoads(load.Loads):
     farm_load_fields = _farm_load_fields
-    farm_load_parser = _farm_load_parser
+
+    @marshal_with(_farm_load_fields)
+    def get(self):
+        loads = models.FarmLoad.query.all()
+        return loads
+
+    @marshal_with(farm_load_fields)
+    def post(self):
+        args = self.load_parser.parse_args()
+        load = models.FarmLoad(**args)
+        if load.departure_time:
+            load.departure_time = parser.parse(load.departure_time)
+        if load.arrival_time:
+            load.arrival_time = parser.parse(load.arrival_time)
+        try:
+            db_session.add(load)
+            db_session.commit()
+        except IntegrityError, e:
+            db_session.rollback()
+            return {'error' : e.message}, 409
+        return load
     
 class FarmLoad(load.Load):
     farm_load_fields = _farm_load_fields
-    farm_load_parser = _farm_load_parser
+
+    @marshal_with(_farm_load_fields)
+    def get(self, id):
+        load = models.FarmLoad.query.get(id)
+        if not load:
+            return abort(404)
+        return load
+
+    def delete(self, id):
+        load = models.FarmLoad.query.get(id)
+        if not load:
+            return abort(404)
+        db_session.delete(load)
+        db_session.commit()
+        return '', 204
 
 class FarmLoadLugs(load.LoadLugs):
-    pass
+    @marshal_with(simple_lug_fields)
+    def get(self, id):
+        load = models.FarmLoad.query.get(id)
+        if not load:
+            return abort(404)
+        return load.lugs
