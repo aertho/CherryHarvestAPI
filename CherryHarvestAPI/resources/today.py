48c6231950121f83dd7f2a9694d2acb084a1ef34
aertho <thomasae@utas.edu.au>
2016-01-09 23:35:31 +1100

    totday resource added

--- CherryHarvestAPI/resources/today.py
+++ CherryHarvestAPI/resources/today.py
@@ -10,7 +10,7 @@ class Today(Resource):
         blocks = [{'variety' : b.variety, 'weight_today': sum([l.weight for l in Lug.query.all() if l.block == b and l.orchard_load.arrival_time == date.today()])} for b in Block.query.all()]
         total_today = sum([l.net_weight for l in OrchardLoad.query.all() if l.arrival_time.date() == date.today()])
         loads = [{'time' :l.arrival_time, 'weight' : l.net_weight} for l in OrchardLoad.query.all() if l.arrival_time.date() == date.today()]
-        pickers = [{'{} {}'.format(p.first_name, p.last_name): p.daily_total()} for p in Picker.query.all() if p.daily_total()]
+        pickers = [{'name':'{} {}'.format(p.first_name, p.last_name), 'weight':p.daily_total()} for p in Picker.query.all() if p.daily_total()]
         return {'total_today' : total_today,
                 'blocks': blocks,
                 'loads': loads,
