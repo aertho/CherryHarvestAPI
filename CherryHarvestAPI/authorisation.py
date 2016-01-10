158d16d9983f6dd63189662b2e98339019db6bbd
aertho <thomasae@utas.edu.au>
2016-01-10 11:41:43 +1100

    fixed authorisation

--- CherryHarvestAPI/authorisation.py
+++ CherryHarvestAPI/authorisation.py
@@ -7,6 +7,6 @@ auth = HTTPBasicAuth()
 @auth.verify_password
 def verify_password(username, password):
     user = User.query.filter_by(username=username).first()
-    if not user or user.check_password(password):
+    if not user or not user.check_password(password):
         return False
     return True
